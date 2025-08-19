c207807a052003062a9e0e811be80b260c7c9884
kimminu7 <rock9024@naver.com>
2025-08-19 15:30:22 +0900

    프로젝트 초기 설정

---
+++ src/main/java/org/example/jwtproeject/JwtproejectApplication.java
@@ -0,0 +1,13 @@
+package org.example.jwtproeject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtproejectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtproejectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/jwtproeject/JwtproejectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jwtproeject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtproejectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
